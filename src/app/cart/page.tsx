8133d79df5ed22b8bdaee3553d919493a933ce26
Eric Soares <ericsoareslopesdev@gmail.com>
2023-10-06 14:49:14 -0300

    refector: add valueCart component

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { devices } from "../global.style";
 import Link from "next/link";
 import { converterParaReal } from "@/hooks/useConversor";
+import ValuesCart from "@/components/unique-pages/cart/valueCart";
 
 const Conteiner = styled.div`
   width: 100%;
@@ -130,63 +131,7 @@ const PriceAndButton = styled.div`
 `;
 
 // conteiner de configuração toral do carrinho
-const PaymentConteiner = styled.div`
-  width: 500px;
-  height: auto;
-  display: block;
-  justify-content: center;
-  align-items: center;
-`;
-const FinishBuy = styled.div`
-  width: 500px;
-  height: 500px;
-  display: block;
-  background-color: ${(props) => props.theme.colors.white};
-  padding: 20px;
-`;
-const Text = styled.div`
-  width: 100%;
-  height: 50px;
-  font-size: 12px;
-`;
-const Section = styled.div`
-  width: 100%;
-  height: 350px;
-  display: flex;
-  flex-direction: column;
-  align-items: end;
-  padding-top: 10px;
-  h1 {
-    font-size: 18px;
-  }
-`;
 
-const AlignBtn = styled.div`
-  width: 100%;
-  height: auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const PayBnt = styled.button`
-  width: 80%;
-  height: 50px;
-  border: none;
-  border-radius: 12px;
-  font-weight: bolder;
-  font-size: 14px;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 14px;
-  background-color: ${(props) => props.theme.colors.blackBlue};
-  color: ${(props) => props.theme.colors.white};
-  transition: ${(props) => props.theme.transition.transition};
-  &:hover {
-    background-color: ${(props) => props.theme.colors.headerBg2};
-  }
-`;
 
 export default function Cart() {
   //pega os itens do localStorage e converte em Object, tornando-se o carrinho
@@ -241,22 +186,6 @@ export default function Cart() {
       </>
     );
   }
-  // função para pegar o valor total do carrinho
-  function getTotalValue() {
-    let index: number = 0;
-    let totalValue: number[] = [];
-    let finalValue: number = 0;
-    cartArray.map((item: any) => {
-      //checando se o item está marcado
-      if (item.cheked) {
-        // se o item estiver marcado, adicionamos à compra final
-        totalValue[index] = item.price_in_cent * item.quatity;
-        finalValue = totalValue[index] + finalValue;
-      }
-      index++;
-    });
-    return converterParaReal(finalValue);
-  }
 
   return (
     <main>
@@ -269,20 +198,7 @@ export default function Cart() {
             <ProductConteiner>
               <div>{getCart()}</div>
             </ProductConteiner>
-            <PaymentConteiner>
-              <FinishBuy>
-                <Text>
-                  <h1>Resumo do Pedido</h1>
-                </Text>
-                <Section>
-                  <h1>Total</h1>
-                  <p>{getTotalValue()}</p>
-                </Section>
-                <AlignBtn>
-                  <PayBnt>Finalizar Compra</PayBnt>
-                </AlignBtn>
-              </FinishBuy>
-            </PaymentConteiner>
+            {ValuesCart(cartArray)}
           </MidConteiner>
         </BigConteinerToAlign>
       </Conteiner>
