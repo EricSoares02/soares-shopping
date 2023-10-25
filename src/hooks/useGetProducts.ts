a832180fc6dd000f268b19a89a388e03c3c535d2
Eric Soares <ericsoareslopesdev@gmail.com>
2023-10-25 16:06:03 -0300

    refactor: change environment variable name

--- src/hooks/useGetProducts.ts
+++ src/hooks/useGetProducts.ts
@@ -2,8 +2,8 @@ import { api } from "@/services/api";
 import { useQuery } from "react-query";
 
 const getProducts = async () => {
-  const url = process.env.URL_API;
-  console.log(url)
+   const url = process.env.REACT_APP_URL;
+  console.log('url é:', url)
   return api
     .get( url || 'http://localhost:3333/produtos/')
     .then((response) => response.data);
